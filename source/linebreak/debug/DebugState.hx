34a60920b343adeff1880c7e4bf13ce7414df85c
IdealistCat <mattallan.h@gmail.com>
2025-01-11 23:11:23 -0600

    Project.hxp file (fnf :)) and a debug state i guess lol

---
+++ source/linebreak/debug/DebugState.hx
@@ -0,0 +1,16 @@
+package linebreak.debug;
+
+import flixel.FlxState;
+
+class DebugState extends FlxState
+{
+	override public function create()
+	{
+		super.create();
+	}
+
+	override public function update(elapsed:Float)
+	{
+		super.update(elapsed);
+	}
+}
